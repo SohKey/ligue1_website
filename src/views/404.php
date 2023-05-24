574841dfbe997d91856be2a208c5b0513749f107
UTI310 <UTI310@sio.local>
2023-05-24 11:28:23 +0200

    update 404 page

--- src/views/404.php
+++ src/views/404.php
@@ -1,3 +1,6 @@
 <div class="h-screen w-full bg-slate-800 flex justify-center items-center">
-    <h1 class="text-slate-100">404 bad gateway</h1>
+    <div class="text-white">
+    <h1 class="text-xl font-bold">404 bad gateway</h1>
+    <p>get back to home: <a class="text-blue-700 hover:text-blue-800" href="/">here</a></p>
+    </div>
 </div>
